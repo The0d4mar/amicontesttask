37b57dfcb3f879244ffa612797c0d7fdfb9aae4f
Vladimir Gornyy <112384479+The0d4mar@users.noreply.github.com>
2026-01-09 22:33:58 +0300

    fix

--- src/components/ResetGame.tsx
+++ src/components/ResetGame.tsx
@@ -9,13 +9,14 @@ interface ResetGameProps{
 const ResetGame:FC<ResetGameProps> = ({onCancel, onConfirm}) => {
   return (
     <div className='resetModal'>
+      <div className='resetModal__cont'>
         <h2 className='resetModal__header'>Вы уверены, что хотите обнулить игру?</h2>
 
         <div className='resetModal__btns'>
-            <button className="resetModal__btn" onClick={onConfirm}>Да</button>
             <button className="resetModal__btn" onClick={onCancel}>Нет</button>
+            <button className="resetModal__btn" onClick={onConfirm}>Да</button>
         </div>
-      
+      </div>
     </div>
   );
 };
